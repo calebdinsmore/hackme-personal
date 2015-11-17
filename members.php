e0beb88963d6416e4e120c32e825a866f254599a
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-11-17 00:26:13 -0500

    Committing to update server - 1.4&1.5 Complete

--- members.php
+++ members.php
@@ -31,7 +31,7 @@
 				if ($passwordHash != $info['pass']) {
 					$query = sprintf("UPDATE users SET log_attempts = %d WHERE username = '".$_POST['username']."'", $info['log_attempts'] + 1);
 					mysql_query($query)or die(mysql_error());
-					print($passwordHash);
+					print($passwordHash."\n");
 					print($info['pass']);
 					die(sprintf('<p>Incorrect password, please try again. Number of login attempts: %d</p>', $info['log_attempts'] + 1));
 				} else if($info['log_attempts'] > 0) {
