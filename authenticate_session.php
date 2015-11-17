34a89a4107e2e992d132d601bac759ab2ac571aa
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-11-17 02:51:24 -0500

    Committing to update server - 1.2&1.4&1.5 Complete

--- authenticate_session.php
+++ authenticate_session.php
@@ -1,9 +1,9 @@
 <?php
-	function valid_session($cookie)
+	function valid_session()
 	{
-    $check = mysql_query("SELECT * FROM users WHERE username = '".$cookie['hackme']."'")or die(mysql_error());
+    $check = mysql_query("SELECT * FROM users WHERE username = '".$_COOKIE['hackme']."'")or die(mysql_error());
     $info = mysql_fetch_array($check);
-    if (password_verify($info['session'], $cookie['hackmesess']) || hash_equals($info['session'], "nosession"))
+    if (password_verify($info['session'], $_COOKIE['hackmesess']) || hash_equals($info['session'], "nosession"))
     {
       return 1;
     } else {
--- header.php
+++ header.php
@@ -8,8 +8,7 @@
 					include('authenticate_session.php');
 					$check = mysql_query("SELECT * FROM users WHERE username = '".$_COOKIE['hackme']."'")or die(mysql_error());
 					$info = mysql_fetch_array($check);
-					echo valid_session($_COOKIE);
-					if(!password_verify($info['session'], $_COOKIE['hackmesess'])){?>
+					if(!valid_session()){?>
 				<li><a href="index.php">Login</a></li>
 				<li><a href="register.php">Register</a></li>
         <?php
