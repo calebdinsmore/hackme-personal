bcc2a08b83156f56eed9dc9b192ff24f558efd41
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-11-17 04:47:31 -0500

    Committing to update server - 1.1&1.2&1.4&1.5 Complete--RSA TEST

--- submit_form.js
+++ submit_form.js
@@ -1,4 +1,10 @@
 function submitform()
 {
-  //document.getElementById("UN-login").value = "poop";
+  var public_key = "-----BEGIN PUBLIC KEY-----\
+	MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDD3aqfqP0Wj0MK/SOq5HFZn8jO\
+	ooczlRSz9qAFZOKOuUVPzd7oGH1iuWJwts+i3m5TL5J3RzcDxM6rioSVF//TB383\
+	cDtKvaTgpW+ulRTvIXUbNDa6a5s7TPZvrdR1C8eRHW76UDxhE/K9/K+iW69U6mCJ\
+	YG5gElQTvRTq3i7RLQIDAQAB\
+	-----END PUBLIC KEY-----<"
+  console.log(public_key);
 }
