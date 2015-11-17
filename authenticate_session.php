af4a51c02d08a217a3cc983a6462e83871a9330f
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-11-17 02:50:01 -0500

    Committing to update server - 1.2&1.4&1.5 Complete

--- authenticate_session.php
+++ authenticate_session.php
@@ -3,7 +3,7 @@
 	{
     $check = mysql_query("SELECT * FROM users WHERE username = '".$cookie['hackme']."'")or die(mysql_error());
     $info = mysql_fetch_array($check);
-    if (password_verify($info['session'], $cookie['hackmesess'])) //|| hash_equals($info['session'], "nosession");
+    if (password_verify($info['session'], $cookie['hackmesess']) || hash_equals($info['session'], "nosession"));
     {
       return 1;
     } else {
