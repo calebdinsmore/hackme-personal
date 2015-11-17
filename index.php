aed79453bb710fcf7f7df3301e25af3cb95398a0
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-11-17 04:29:35 -0500

    Committing to update server - 1.1&1.2&1.4&1.5 Complete--RSA TEST

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 				<table>
 					<tr> <td> Username </td> <td> <input type="text" name="username" /> </td> </tr>
 					<tr> <td> Password </td> <td> <input type="password" name="password" /> </td>
-                    <td> <input type="button" name = "submit" value="Login" onclick="javascript: submitform()"/> </td></tr>
+                    <td> <input type="submit" name = "submit" value="Login" onclick="javascript: submitform()"/> </td></tr>
 				</table>
 				</form>
 
