fdf4c031dc2cf21c927bd1d6fca55bd227818e92
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-11-17 03:02:37 -0500

    Committing to update server - 1.2&1.4&1.5 Complete

--- logout.php
+++ logout.php
@@ -1,4 +1,7 @@
 <?php
+// Connects to the Database
+include('connect.php');
+connect();
 $query = "UPDATE users SET session = '"."nosession"."' WHERE username = '".$_COOKIE['hackme']."'";
 mysql_query($query)or die(mysql_error());
 setcookie (hackme, "", time() - 3600);
