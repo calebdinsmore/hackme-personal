284539c3393d93e17c7dacc49a1d2eecd09b085e
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-11-17 02:56:33 -0500

    Committing to update server - 1.2&1.4&1.5 Complete

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 	include('authenticate_session.php');
 	include('connect.php');
 	connect();
-	if(valid_session()){
+	$check = mysql_query("SELECT * FROM users WHERE username = '".$_COOKIE['hackme']."'")or die(mysql_error());
+	$info = mysql_fetch_array($check);
+	if (password_verify($info['session'], $_COOKIE['hackmesess']) || hash_equals($info['session'], "nosession"))
+	{
 		header("Location: members.php");
 	}
 ?>
