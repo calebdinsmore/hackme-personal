55dca608f65914c0be663577f7adc802a70ecbad
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-11-17 02:46:59 -0500

    Committing to update server - 1.2&1.4&1.5 Complete

--- authenticate_session.php
+++ authenticate_session.php
@@ -3,7 +3,6 @@
 	{
     $check = mysql_query("SELECT * FROM users WHERE username = '".$cookie['hackme']."'")or die(mysql_error());
     $info = mysql_fetch_array($check);
-    print($info['pass']);
     return password_verify($info['session'], $cookie['hackmesess']); //|| hash_equals($info['session'], "nosession");
 	}
 ?>
