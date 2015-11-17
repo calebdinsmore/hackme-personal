757dd7e59bd7c91632a1cafd58b43d311778861b
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-11-17 18:12:05 -0500

    Committing to update server - 1.1&1.2&1.4&1.5 Complete--RSA TEST

--- connect.php
+++ connect.php
@@ -17,32 +17,23 @@
       return 0;
     }
 	}
-	//I am well aware that putting this function in this file is utter nonsense. But it's late, and I don't care. :P
-	function my_decrypt($to_decrypt)
-	{
-		$pubkey = '-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCjFLYEjRkUPeCFbTqQZaCeq5GU
-xieUakf4MAkfBqWo9yZjAM0biaBdDmifDkCnKtJLBkmUnKFpSAQRxGY71+1Ln+Vi
-GrGzSRpItMxEnpdPdP9Hn0UfgeZ451AOFnhvC8n/xJvYfSpZhqD7eRMFE9F750xT
-N89VsJLYT9jGxoUThwIDAQAB
------END PUBLIC KEY-----
-';
-		$priv_key = '-----BEGIN RSA PRIVATE KEY-----
-MIICXgIBAAKBgQD0W8Wj5YToT4RjAaX+EX+k8Sc7oCSYJVRrZmiubDCUTQm93kCjvEPEBbPojutU
-GMvbshJqTKKeY7F1YYLfrMVbxcCLr1XN1OD9+jKUeE5ym9cIBoJ82fN5CI/ZQ3+ssGmawRFQ89Z5
-fyGB+LWJga9t4z7xlTYjlBhcyNEsHJ8tyQIDAQABAoGBAKP+PzsKm1MJorCLd6p2dfLtgUYL6ONP
-EkPt+80rgMLWnPYXBcydWeFhbmdiG19aMN5luOQsQGsKPxum8J1KpzvrT9hiFugMja1z1alqxpKa
-yG7bFvOfWdcwHIewUmaOAxz2mKe/QJ+pOCE9SgvQvznTMVCAdzB9ObIuM0q/zVi9AkEA/ftr98l4
-FmGuZNqExfAqy+8JXrt7AL6opob6f0axBtNLPI8fw8jLWXNIcrCi/BBFoEFwyvvtpJqTgCgdyNeW
-AwJBAPZMxtDsb21Qxshmb7JGdI09Id2udtHkpWS+J4vIBtRXE92fH53KKG9zLfksuSck7MZB8oLE
-FFlmGb7+KCJu+UMCQQCYwLZW+Rz4mRdCIQrp4WBb9xAzoZ6A/CqCvXu7QNEHwdzmN05rekCTM/rG
-v+XGpCK8F5+29X4gGbfMxFPlj4PxAkACcGYzoXPFCFy/lUwb3ti+oVFZiaXBlFsS8VMg7j0rEyWu
-Nyov/NWDrQdShV/cBGCX4gVNyDVPYVR18LxjAuhTAkEAmVvPDN41feEeBoppCa63+JPMFt14DSG1
-NCg84L21MnwI7fTowVRUjcmf4UT2yC22X3oZTJ9QVqNw/dMFSReeYA==
------END RSA PRIVATE KEY-----';
+	
+		function publicKeyToHex($privatekey) {
 
-$rsa = new Crypt_RSA();
-$rsa->loadKey($priv_key); // private key
-echo $rsa->decrypt(base64_decode($to_decrypt));
+			$rsa = new Crypt_RSA();
+
+			$rsa->loadKey($privatekey);
+			$raw = $rsa->getPublicKey(CRYPT_RSA_PUBLIC_FORMAT_RAW);
+			return $raw['n']->toHex();
+		}
+
+	function decrypt($privatekey, $encrypted) {
+		$rsa = new Crypt_RSA();
+
+		$encrypted=pack('H*', $encrypted);
+
+		$rsa->loadKey($privatekey);
+		$rsa->setEncryptionMode(CRYPT_RSA_ENCRYPTION_PKCS1);
+		return $rsa->decrypt($encrypted);
 	}
 ?>
