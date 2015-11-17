b41e67a6fd00ae5a5799aa16548e431b9acd044d
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-11-17 06:08:18 -0500

    Committing to update server - 1.1&1.2&1.4&1.5 Complete--RSA TEST

--- connect.php
+++ connect.php
@@ -32,8 +32,6 @@
 		$pubKey = $pubKey["key"];
 		file_put_contents("privkey.pem", $privKey);
 		file_put_contents("pubkey.pem", $pubKey);
-		echo $privKey."<br/>";
-		echo $pubKey;
 		if (openssl_private_decrypt(base64_decode($to_decrypt), $decrypted, $priv_key)){
 			echo 'true';
 		} else {
