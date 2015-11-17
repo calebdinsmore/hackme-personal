264b87206a35276c68726edab5c3e6235d39004d
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-11-17 17:09:43 -0500

    Committing to update server - 1.1&1.2&1.4&1.5 Complete--RSA TEST

--- index.php
+++ index.php
@@ -81,9 +81,9 @@ function submitform()
   var username = document.getElementById("UN-login");
   var password = document.getElementById("PW-login");
 	var publickey = "<?=publicKeyToHex($privatekey)?>";
+	var rsakey = new RSAKey();
   username.value = rsakey.encrypt($('#UN-login').val());
   password.value = rsakey.encrypt($('#PW-login').val());
-	var rsakey = new RSAKey();
   rsakey.setPublic(publickey, "10001");
 	//var enc = rsakey.encrypt($('#plaintext').val());
 
