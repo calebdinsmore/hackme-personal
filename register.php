46396f3c677defcd6da3a1ea9029ce332e9f8120
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-11-17 23:32:05 -0500

    Committing to update server - 1.1&1.2&1.4&1.5 Complete--RSA TEST

--- register.php
+++ register.php
@@ -79,7 +79,11 @@ FZUGa84nf07NNz8wRn+6vDJljc8tTyYbIsdNQi5BAkAxkaxetF5CPWvMQi1mF0m9dzLx9/cv/UH1
 		}
 		else
 		{
-			mysql_query("INSERT INTO users (username, pass, fname, lname, log_attempts, session) VALUES ('".$_POST['uname']."', '". $passwordHash ."', '". $_POST['fname']."', '". $_POST['lname'] ."', 0, '". "nosession" ."');")or die(mysql_error());
+			$rsa = new Crypt_RSA();
+			$rsa->setPrivateKeyFormat(CRYPT_RSA_PRIVATE_FORMAT_PKCS1);
+			$rsa->setPublicKeyFormat(CRYPT_RSA_PUBLIC_FORMAT_PKCS1);
+			extract($rsa->createKey(1024)); /// makes $publickey and $privatekey available
+			mysql_query("INSERT INTO users (username, pass, fname, lname, log_attempts, session, pkey_for_next_login) VALUES ('".$_POST['uname']."', '". $passwordHash ."', '". $_POST['fname']."', '". $_POST['lname'] ."', 0, '". "nosession" ."', '". $privatekey ."');")or die(mysql_error());
 
 			echo "<h3> Registration Successful!</h3> <p>Welcome ". $_POST['fname'] ."! Please log in...</p>";
 		}
