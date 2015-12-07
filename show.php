75c8dc7f0d4336eb0b64c7c2f405f4e2f3311f7a
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-12-07 17:33:15 -0500

    Part 2 stuff

--- show.php
+++ show.php
@@ -28,7 +28,7 @@
 	}
 ?>
 <?php
-	echo "SELECT * FROM threads WHERE id = '".$_GET[pid]."'";
+	print("SELECT * FROM threads WHERE id = '".$_GET[pid]."'");
 	$threads = mysql_query("SELECT * FROM threads WHERE id = '".$_GET[pid]."'") or die(mysql_error());
 	while($thisthread = mysql_fetch_array( $threads )){
 ?>
