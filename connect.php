273ce87d678c1a60002bb4313ec02112671ab779
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-12-08 00:06:45 -0500

    Part 1 - reverting back

--- connect.php
+++ connect.php
@@ -2,13 +2,7 @@
 	function connect()
 	{
 		// Connects to the Database
-		#mysql_connect("localhost", "security-project", "69BgYftvzpEH", false, 65536) or die(mysql_error());
-		#mysql_select_db("hackme") or die(mysql_error());
-
-		$mysqli = new mysqli("localhost", "security-project", "69BgYftvzpEH", "hackme");
-		if (mysqli_connect_errno()) {
-    	printf("Connect failed: %s\n", mysqli_connect_error());
-    	exit();
-		}
+		mysql_connect("localhost", "security-project", "69BgYftvzpEH", false, 65536) or die(mysql_error());
+		mysql_select_db("hackme") or die(mysql_error());
 	}
 ?>
