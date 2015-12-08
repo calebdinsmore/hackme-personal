ed596b4f4e56f1a88825e3581e84bc766cdba1a0
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-12-07 23:06:58 -0500

    Part 2 - Trying prepped stmts

--- connect.php
+++ connect.php
@@ -2,7 +2,12 @@
 	function connect()
 	{
 		// Connects to the Database
-		mysql_connect("localhost", "security-project", "69BgYftvzpEH", false, 65536) or die(mysql_error());
-		mysql_select_db("hackme") or die(mysql_error());
+		#mysql_connect("localhost", "security-project", "69BgYftvzpEH", false, 65536) or die(mysql_error());
+		#mysql_select_db("hackme") or die(mysql_error());
+
+		$mysqli = new mysqli("localhost", "security-project", "69BgYftvzpEH", "65536");
+		if ($mysqli->connect_errno) {
+		    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+		}
 	}
 ?>
