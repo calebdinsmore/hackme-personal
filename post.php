2e1818ae7f829d71fe34cf6485d1f820da006866
cjdinsmore <caleb_dinsmore@taylor.edu>
2015-12-09 02:41:07 -0500

    Part 2 - Finished?

--- post.php
+++ post.php
@@ -2,6 +2,7 @@
 // Connects to the Database
 	include('connect.php');
 	connect();
+	require_once '/var/www/html/hackme/csrf-magic/csrf-magic.php';
 
 	//if the login form is submitted
 	if (isset($_POST['post_submit'])) {
